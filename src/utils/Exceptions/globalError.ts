54c9e53d377b9615bdd0b5da1af347b08497eadf
Md. Yasir Aarafat <yasirarafat1995@gmail.com>
2022-03-05 23:26:31 +0600

    fixes

---
+++ src/utils/Exceptions/globalError.ts
@@ -0,0 +1,25 @@
+import express from "express";
+const sendErrorDev = (err: any, res: express.Response) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    statusCode: err.statusCode,
+    errors: err.errors,
+    message: err.message,
+    correlationId: err.correlationId,
+    stack: err.stack,
+  });
+};
+const globalError = (
+  err: any,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  console.log("ssssss", err.statusCode);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  err.correlationId = req.headers["x-correlation-id"];
+  sendErrorDev(err, res);
+};
+
+export { globalError };
